ba855233dfb8f2d328b129d4d7ef6b0db6d9a2e2
Sergio Cuadrado <2sergiocuadrado2000@gmail.com>
2025-02-25 19:26:59 +0100

    [ADD] game-service: dependencies and initial yaml config

---
+++ game-service/src/test/java/com/sergiocuacor/game_service/GameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiocuacor.game_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
