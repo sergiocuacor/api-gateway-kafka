e79e282bd3e55d56804f05cb7f60cd089be854fe
Sergio Cuadrado <2sergiocuadrado2000@gmail.com>
2025-03-03 12:49:01 +0100

    game-service: add kafka_default config (application.yaml), dependencies

---
+++ elegibility-service/src/main/java/com/sergiocuacor/elegibility_service/ElegibilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sergiocuacor.elegibility_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElegibilityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElegibilityServiceApplication.class, args);
+	}
+
+}
---
+++ elegibility-service/src/test/java/com/sergiocuacor/elegibility_service/ElegibilityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiocuacor.elegibility_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElegibilityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
